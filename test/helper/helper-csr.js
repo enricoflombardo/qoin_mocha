d5fedaddaea04f7c1b0199047b184f25f92452dc
ahmad m maruf <ahmadyagami@gmail.com>
2022-10-10 14:58:03 +0700

    update dari id-akta ke semua id-id

--- test/helper/helper-csr.js
+++ test/helper/helper-csr.js
@@ -7,7 +7,7 @@ export default async function Getid(){
     if (typeof response.data.data.id == "undefined") {
         IDrespons = "3"
     }else{
-        IDrespons = response.data.data.id
+        IDrespons = response.data.data.list[0].id
         // IDrespons = ""
     }
     
@@ -17,25 +17,57 @@ export default async function Getid(){
 
 export async function Getissuer_code(){
     const response = await QoinAPI.penerbit() //hit API
-    let IDrespons = response.data.data.IssuerCode
-    return IDrespons 
+    let IDrespons = "3"
+    
+    if (typeof response.data.data.total_items == 0) {
+        IDrespons = "3"
+    }else{
+        IDrespons = response.data.data.list[0].id
+        // IDrespons = ""
+    }
+    
+    return IDrespons
 };
 
 export async function Getissuer_name(){
     const response = await QoinAPI.penerbit() //hit API
-    let IDrespons = response.data.data.IssuerName
+    let IDrespons = "3"
+    
+    if (typeof response.data.data.total_items == 0) {
+        IDrespons = "3"
+    }else{
+        IDrespons = response.data.data.list[0].id
+        // IDrespons = ""
+    }
+    
     return IDrespons 
 };
 
 export async function Getid_voucher(){
     const response = await QoinAPI.voucher() //hit API
-    let IDrespons = response.data.data.id
-    return IDrespons 
+    let IDrespons = "3"
+    
+    if (typeof response.data.data.total_items == 0) {
+        IDrespons = "3"
+    }else{
+        IDrespons = response.data.data.list[0].id
+        // IDrespons = ""
+    }
+    
+    return IDrespons
 };
 
 export async function Getprefix_code(){
     const response = await QoinAPI.voucher() //hit API
-    let IDrespons = response.data.data.PrefixCode
+    let IDrespons = "3"
+    
+    if (typeof response.data.data.total_items == 0) {
+        IDrespons = "3"
+    }else{
+        IDrespons = response.data.data.list[0].id
+        // IDrespons = ""
+    }
+    
     return IDrespons 
 };
 
@@ -43,7 +75,7 @@ export async function Getid_program(){
     const response = await QoinAPI.listbansos() //hit API
     let IDrespons = "3"
     
-    if (response.data.data.total_items == 0) {
+    if (typeof response.data.data.total_items == 0) {
         IDrespons = "3"
     }else{
         IDrespons = response.data.data.list[0].id
@@ -57,7 +89,7 @@ export async function Getid_user(){
     const response = await QoinAPI.listbansospenerima() //hit API
     let IDrespons = "3"
     
-    if (response.data.data.total_items == 0) {
+    if (typeof response.data.data.total_items == 0) {
         IDrespons = "3"
     }else{
         IDrespons = response.data.data.list[0].id
@@ -71,7 +103,7 @@ export async function Getid_penerima(){
     const response = await QoinAPI.listbansospenerima() //hit API
     let IDrespons = "3"
     
-    if (response.data.data.total_items == 0) {
+    if (typeof response.data.data.total_items == 0) {
         IDrespons = "3"
     }else{
         IDrespons = response.data.data.list[0].id
@@ -85,7 +117,7 @@ export async function Getid_bansos(){
     const response = await QoinAPI.bansosuser() //hit API
     let IDrespons = "3"
     
-    if (response.data.data.total_items == 0) {
+    if (typeof response.data.data.total_items == 0) {
         IDrespons = "3"
     }else{
         IDrespons = response.data.data.vouchers[0].m_bansos_id
@@ -99,7 +131,7 @@ export async function kode_voucher(){
     const response = await QoinAPI.bansosuser() //hit API
     let IDrespons = "3"
     
-    if (response.data.data.total_items == 0) {
+    if (typeof response.data.data.total_items == 0) {
         IDrespons = "3"
     }else{
         IDrespons = response.data.data.vouchers[0].kode_voucher
