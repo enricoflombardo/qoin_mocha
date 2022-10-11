29218a7771e25a3a8fd6864a25b99bcd5518766a
Ridwan Hananto Aji Arifin <ridwanhanantoaji@gmail.com>
2022-10-11 13:54:37 +0700

    Keterangan

---
+++ test/helper/helper-sentraLokasi.js
@@ -0,0 +1,26 @@
+import QoinAPI from '$root/pages/SentraLokasi_Kategori.api'; //import endpoint API
+import QoinAPI2 from '$root/pages/SentraLokasi_Lokasi.api'; //import endpoint API
+
+export default async function Getid(){
+    const response = await QoinAPI.get_lokasi() //hit API
+    let IDrespons = "3"
+    
+    // validate api
+    if (response.data.id == "undefined") {
+        IDrespons = "3"
+    }else{
+        IDrespons = response.data.id
+        // IDrespons = ""
+    }
+    
+    return IDrespons
+}
+
+export async function Getid_Lokasi(){
+    const response = await QoinAPI2.get_lokasi() //hit API
+    let IDrespons = response.data.data.list[0].id
+    return IDrespons 
+}
+
+ 
+// export default Getid
