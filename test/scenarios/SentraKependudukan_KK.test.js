04d3bf7bf2446925855995d696538e54d2e2bb96
Enrico Lombardo <enricoflombardo5@gmail.com>
2022-10-13 16:30:39 +0700

    add check result

--- test/scenarios/SentraKependudukan_KK.test.js
+++ test/scenarios/SentraKependudukan_KK.test.js
@@ -76,9 +76,6 @@ describe('Web', () => {
         
             //result
             assert.equal(response.status, 200);
-            // assert.equal(response.data.list[0].id, id.id);
-
-            // console.log(response.data.data.list);
 
             //schema
             expect(response.data).to.be.jsonSchema(schema.VALIDATE_GET_INDEX_BY_ID_SCHEMA)
@@ -88,9 +85,42 @@ describe('Web', () => {
         const response = await QoinAPI.updatekk(id, data.VALID_UPDATEKK, token) //hit API
 
         assert.equal(response.status, 200);
-        // assert.equal(response.data.data.nama_pemohon.toLowerCase(), data.VALID_UPDATEKK.data.nama_pemohon.toLowerCase());
-        // assert.equal(response.data.data.kecamatan_domisili, data.VALID_UPDATEKK.data.kecamatan_domisili);
-        // assert.equal(response.data.data.kota_domisili_id, data.VALID_UPDATEKK.data.kota_domisili_id);
+
+        // console.log(response.data)
+
+        //Check Data
+        
+        const data_res = Object.fromEntries(
+            Object.entries(response.data.data).map(([key, value]) => [key, typeof value == 'string' ? value.toLowerCase() : value])
+          );
+
+        const data_in = Object.fromEntries(
+            Object.entries(data.VALID_UPDATEKK).map(([key, value]) => [key, typeof value == 'string' ? value.toLowerCase() : value])
+          );
+        
+        //check Data
+        assert.deepEqualExcluding(data_in, data_res, ['id', 'updatebyid', 'updated_at', 'client_id', 'created_by', "jenis_permohonan", 
+        "jumlah_anggota", 
+        "jam_kedatangan", 
+        "jumlah_anggota", 
+        "kecamatan_domisili_id", 
+        "kecamatan_id", 
+        "kelurahan_domisili_id", 
+        "kelurahan_id", 
+        "kota_domisili_id", 
+        "kota_id", 
+        "lokasi_id", 
+        "m_user_id", 
+        "no_registrasi", 
+        "provinsi_domisili_id", 
+        "provinsi_id", 
+        "rt", 
+        "rw", 
+        "rt", 
+        "rw", 
+        "status", 
+        "tanggal_kedatangan", 
+        "tanggal_permohonan"])
     
         expect(response.data).to.be.jsonSchema(schema.VALIDATE_UPDATE_KK_SCHEMA) //Call JSON Schema
     });
@@ -99,6 +129,8 @@ describe('Web', () => {
         const response = await QoinAPI.updatestatusverif(id, data.VALID_STATUSVERIFKK, token)
         
         assert.equal(response.status, 200);
+
+        assert.equal(response.data.data.notes.toLowerCase(), data.VALID_STATUSVERIFKK.notes.toLocaleLowerCase());
     
         expect(response.data).to.be.jsonSchema(schema.VALIDATE_UPDATE_STATUSVERIF_SCHEMA) //Call JSON Schema
     });
@@ -107,6 +139,8 @@ describe('Web', () => {
         const response = await QoinAPI.updatestatus(id, data.VALID_STATUSKK, token)
 
         assert.equal(response.status, 200);
+
+        assert.equal(response.data.data.status.toLowerCase(), data.VALID_STATUSKK.status.toLowerCase());
     
         expect(response.data).to.be.jsonSchema(schema.VALIDATE_UPDATE_STATUS_SCHEMA) //Call JSON Schema
     });
@@ -115,6 +149,8 @@ describe('Web', () => {
         const response = await QoinAPI.updatejadwal(data.VALID_UPDATE_JADWAL_KK, token) //hit API
 
         assert.equal(response.status, 200);
+
+        assert.equal(response.data.data.tanggal_kedatangan, data.VALID_UPDATE_JADWAL_KK.tanggal_kedatangan);
     
         expect(response.data).to.be.jsonSchema(schema.VALIDATE_UPDATE_JADWAL_SCHEMA) //Call JSON Schema
     });
@@ -123,6 +159,17 @@ describe('Web', () => {
         const response = await QoinAPI.updateanggota(id, id_anggota, data.VALID_UPDATE_ANGGOTA, token) //hit API
 
         assert.equal(response.status, 200);
+
+        //Check Data
+        const data_res = Object.fromEntries(
+            Object.entries(response.data.data).map(([key, value]) => [key, typeof value == 'string' ? value.toLowerCase() : value])
+          );
+
+        const data_in = Object.fromEntries(
+            Object.entries(data.VALID_UPDATE_ANGGOTA).map(([key, value]) => [key, typeof value == 'string' ? value.toLowerCase() : value])
+          );
+
+        assert.deepEqualExcluding(data_in, data_res, ['id'])
     
         expect(response.data).to.be.jsonSchema(schema.VALIDATE_UPDATE_ANGGOTA_SCHEMA) //Call JSON Schema
     });
@@ -131,6 +178,17 @@ describe('Web', () => {
         const response = await QoinAPI.updatekonfirmasi(id, data.VALID_STATUSKONFIRKK, token)
 
         assert.equal(response.status, 200);
+
+        //Check Data
+        const data_res = Object.fromEntries(
+            Object.entries(response.data.data).map(([key, value]) => [key, typeof value == 'string' ? value.toLowerCase() : value])
+          );
+
+        const data_in = Object.fromEntries(
+            Object.entries(data.VALID_STATUSKONFIRKK).map(([key, value]) => [key, typeof value == 'string' ? value.toLowerCase() : value])
+          );
+
+        assert.deepEqualExcluding(data_in, data_res, ['id', 'updatebyid', 'updated_at'])
         
         expect(response.data).to.be.jsonSchema(schema.VALIDATE_UPDATE_STATUSKONFIR_SCHEMA) //Call JSON Schema
     });
